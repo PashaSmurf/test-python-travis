a514770f7b1e47793209c84126430a47c0c7f561
Pavel Liakhouski <Pavel.Liakhouski1@ibm.com>
2020-11-15 09:10:03 -0800

    Add env-var.py file
    Signed-off-by: Pavel Liakhouski <Pavel.Liakhouski1@ibm.com>

--- travis_project/app.py
+++ travis_project/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
+from travis_project.env_var import TEST_SECRET_2
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello():
-    return "Hello World!"
+    return 'Hello World!' + TEST_SECRET_2
 
 
 if __name__ == '__main__':
---
+++ travis_project/env_var.py
@@ -0,0 +1,4 @@
+import os
+
+TEST_SECRET_1 = os.environ.get('TEST_SECRET_1', 'default_value_1')
+TEST_SECRET_2 = os.environ.get('TEST_SECRET_2', 'default_value_2')
